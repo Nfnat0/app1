d4604c5467bcd322ac56430fcd3444cd015c454d
Nfnat0 <mjtmwm@gmail.com>
2022-03-31 06:28:06 +0900

    PC版の大体の見た目を作成

--- game/models.py
+++ game/models.py
@@ -1,3 +1,12 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+
+
+class Post(models.Model):
+    no = models.IntegerField()
+    text = models.TextField()
+    img = models.ImageField()
+
+    def __str__(self):
+        return f'{self.no}:{self.text} '
--- game/views.py
+++ game/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from game.models import Post
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementClickInterceptedException
 
-def post_list(request):
-    posts = [1, 2, 3]
+def post_list(request, posts=None):
+    if not posts:
+        posts = [Post(no=i, text='test') for i in range(1,10)]
+        return render(request, 'game/post_list.html', {'posts': posts})
+
+
+    posts = [Post(no=i, text='test') for i in range(1,10)]
 
     return render(request, 'game/post_list.html', {'posts': posts})
